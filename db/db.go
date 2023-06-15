e6f392c8bc68c90454b85c80cf8211bf1c3d7f49
Paulo Victor <Paulo.victor.ig@hotmail.com>
2023-06-15 16:48:14 -0300

    refatorando o codigo e pagina de cadastro implmentada

--- db/db.go
+++ db/db.go
@@ -5,7 +5,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func conectaComBancoDeDados() *sql.DB {
+func ConectaComBancoDeDados() *sql.DB {
 	conexao := "user=postgres port=5433 dbname=paulo_loja password=1234 host=localhost sslmode=disable"
 
 	db, err := sql.Open("postgres", conexao)
---
+++ routes/routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"net/http"
+	"paulo/controllers"
+)
+
+func CarregaRotas() {
+	http.HandleFunc("/", controllers.Index)
+	http.HandleFunc("/new", controllers.New)
+	http.HandleFunc("/insert", controllers.Insert)
+}
