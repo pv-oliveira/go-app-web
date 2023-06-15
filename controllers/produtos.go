9001f48ba9fcce51ab0f8770b772f4aa0e7235b7
Paulo Victor <Paulo.victor.ig@hotmail.com>
2023-06-15 19:04:42 -0300

    Atualizando e editando produtos

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -45,5 +46,41 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 func Delete(w http.ResponseWriter, r *http.Request) {
 	idProduto := r.URL.Query().Get("id")
 	models.DeletaProduto(idProduto)
+	http.Redirect(w, r, "/", 301)
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	idProduto := r.URL.Query().Get("id")
+	fmt.Println(idProduto)
+	produto := models.EditaProduto(idProduto)
+	fmt.Println(produto)
+	temp.ExecuteTemplate(w, "Edit", produto)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		idConvertidaParaInt, err := strconv.Atoi(id)
+		if err != nil {
+			log.Println("Erro na convesão do ID para int:", err)
+		}
+
+		precoConvertidoParaFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na convesão do preço para float64:", err)
+		}
+
+		quantidadeConvertidaParaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na convesão da quantidade para int:", err)
+		}
+
+		models.AtualizaProduto(idConvertidaParaInt, nome, descricao, precoConvertidoParaFloat, quantidadeConvertidaParaInt)
+	}
 	http.Redirect(w, r, "/", 301)
 }
